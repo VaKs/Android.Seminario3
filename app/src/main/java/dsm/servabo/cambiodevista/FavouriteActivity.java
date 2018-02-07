0aea7f6741ec30b47285182c687c058d44dacfa2
Sergio Vacas <vaks@hotmail.es>
2018-02-07 02:40:36 +0100

    añadido el link del boton autor de favoritosañadido el link del boton autor de favoritos

--- app/src/main/java/dsm/servabo/cambiodevista/FavouriteActivity.java
+++ app/src/main/java/dsm/servabo/cambiodevista/FavouriteActivity.java
@@ -1,7 +1,13 @@
 package dsm.servabo.cambiodevista;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import static dsm.servabo.cambiodevista.R.id.bntAutorInfo;
 
 public class FavouriteActivity extends AppCompatActivity {
 
@@ -9,5 +15,16 @@ public class FavouriteActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_favourite);
+
+        Button botonAutor = findViewById(bntAutorInfo);
+        botonAutor.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_VIEW);
+                intent.setData(Uri.parse("http://en.wikipedia.org/wiki/Special:Search?search=Albert_Einstein"));
+                startActivity(intent);
+            }
+        });
+
     }
 }

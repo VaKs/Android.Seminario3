c0bf5ef685550bb15f8346b1979c69c08998d6f0
Sergio Vacas <vaks@hotmail.es>
2018-02-07 03:42:21 +0100

    terminado seminario 3A a excepcion apartado del punto 5: Asocia  un  onClickListener  a  ImageButton.  Al  pulsar  este  botón  deberá  obtenerse  una nueva  cita.  En  estos  momentos,  simplemente  sustituye  el  texto  mostrado  en  ambas etiquetas por los recursos correspondientes a “sample quotation” y “sample author”.

--- app/src/main/java/dsm/servabo/cambiodevista/QuotationActivity.java
+++ app/src/main/java/dsm/servabo/cambiodevista/QuotationActivity.java
@@ -1,13 +1,37 @@
 package dsm.servabo.cambiodevista;
 
+
+import android.annotation.SuppressLint;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.TextView;
+import android.widget.Toast;
+
 
 public class QuotationActivity extends AppCompatActivity {
 
+    ImageButton imageButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quotation);
+
+
+        imageButton = (ImageButton) findViewById(R.id.imageButton);
+
+        imageButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View arg0) {
+                Toast.makeText(QuotationActivity.this,
+                        "ImageButton is clicked!", Toast.LENGTH_SHORT).show();
+               //TextView txtCita = findViewById(R.id.txtCitaHello);
+                //txtCita.setText("sample quotation");
+
+            }
+
+        });
     }
 }
